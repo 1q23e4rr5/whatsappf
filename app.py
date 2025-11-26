dc3a3535c5a1a5c34d4f23263cb83eb3dce609ab
chat_app <amir.malekahmadi89@gmail.com>
2025-11-27 00:59:11 +0330

    Update app.py

--- app.py
+++ app.py
@@ -468,4 +468,5 @@ def admin_logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
